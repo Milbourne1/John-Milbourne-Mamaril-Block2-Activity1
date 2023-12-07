c9e9b037baad2ae94dffd59b142437b2bf16932f
Milbourne1 <jona.mamaril.up@phinmaed.com>
2023-12-07 18:16:23 +0800

    4

--- app/src/main/java/com/mamaril/john/milbourne/block2/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/mamaril/john/milbourne/block2/p1/quiz/MainActivity.kt
@@ -2,10 +2,23 @@ package com.mamaril.john.milbourne.block2.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstNumber: EditText = findViewById(R.id.insrt_First)
+        val secondNumber: EditText = findViewById(R.id.insrt_Second)
+        val thirdNumber: EditText = findViewById(R.id.insrt_Third)
+        val fourthNumber: EditText = findViewById(R.id.insrt_Fourth)
+        val btnAdd: Button = findViewById(R.id.btn_Add)
+        val btnSubtract: Button = findViewById(R.id.btn_Subtract)
+        val btnMultiply: Button = findViewById(R.id.btn_Multiply)
+        val btnDivide: Button = findViewById(R.id.btn_Divide)
+        val txtNumber: TextView = findViewById(R.id.txt_Number)
     }
 }
